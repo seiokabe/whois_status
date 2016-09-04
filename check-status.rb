8339a2b37b290d5e6bb8743c43c68e0e8954e527
Seiichiro Okabe <okabe@Gemini.local>
2016-09-04 21:49:33 +0900

    Update Gemini

--- check-status.rb
+++ check-status.rb
@@ -46,7 +46,7 @@ def CheckStatus(arr)
   error = false
   errDomain = Array.new()
   arr.each{|hash|
-    print hash
+    print hash[:domain]
     error = true unless hash[:status]
     error = true if hash[:available]
     # if hash[:expires_on]
